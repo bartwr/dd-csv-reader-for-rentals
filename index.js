6f0ad94ddc7db2f26fec6ade42c93696666094be
bartwr <mail@bartroorda.nl>
2022-12-01 22:30:00 +0100

    Write CSV file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const { parse } = require("csv-parse");
 const moment = require("moment");
+const { stringify } = require("csv-stringify");
 
 // 0: provider: 'check',
 // 1: 'check:a9a36819-c021-429f-a0a8-78c868a92d46',
@@ -13,7 +14,17 @@ const moment = require("moment");
 // 8: lat_end: '51.89153',
 // 9: lng_end: '4.49378',
 
+const fileNameToImport = '20221201_export_trips_Kievitslaan-2022-06-01-to-2022-12-01.csv';
+
 let allCsvData = [], aggregatedPerWeek = [];
+
+const providers = [
+  "check",
+  "felyx",
+  "gosharing",
+  "donkey",
+  "baqme"
+];
 
 const aggregatePerWeek = (data) => {
   data.forEach(x => {
@@ -26,16 +37,76 @@ const aggregatePerWeek = (data) => {
   return aggregatePerWeek;
 }
 
-fs.createReadStream("./import/20221201_export_trips_Kievitslaan-2022-06-01-to-2022-12-01.csv")
+const generateCsvDataPerWeek = (data) => {
+  let csvData = [];
+  Object.keys(data).forEach(key => {
+    let row = [], providerCounts = data[key];
+    row.week_number = key;
+    Object.keys(providerCounts).forEach((provider) => {
+      row[provider] = providerCounts[provider];
+    });
+
+    csvData.push(row);
+  })
+  return csvData;
+}
+
+const writeToFile = (data) => {
+  const filename = "./export/"+fileNameToImport;
+  const writableStream = fs.createWriteStream(filename);
+
+  const columns = [
+    "week_number",
+    "check",
+    "felyx",
+    "gosharing",
+    "donkey",
+    "baqme",
+    "lime"
+  ];
+
+  const stringifier = stringify({ header: true, columns: columns });
+
+  const getRowDataForCsv = (row) => {
+    let rowDataForCsv = [];
+    rowDataForCsv.push(row.week_number);
+    // Loop all providers
+    providers.forEach(providerName => {
+      // Loop all row values and check if provider is in it
+      let didFoundValue = false;
+      Object.keys(row).forEach(key => {
+        const val = row[key];
+        if(key === providerName) {
+          rowDataForCsv.push(val);
+          didFoundValue = true;
+        }
+      });
+      if(! didFoundValue) {
+        rowDataForCsv.push(0);
+      }
+    });
+    return rowDataForCsv;
+  }
+
+  Object.keys(data).forEach(key => {
+    const rowDataForCsv = getRowDataForCsv(data[key]);
+    stringifier.write(rowDataForCsv);
+  });
+
+  stringifier.pipe(writableStream);
+  console.log("Finished writing data");
+}
+
+fs.createReadStream("./import/"+fileNameToImport)
   .pipe(parse({ delimiter: ",", from_line: 2 }))
   .on("data", function (row) {
-    console.log(row);
     allCsvData.push(row);
   })
   .on("end", function () {
     console.log("finished");
     const perWeek = aggregatePerWeek(allCsvData);
-    console.log(perWeek)
+    const csvDataPerWeek = generateCsvDataPerWeek(perWeek);
+    writeToFile(csvDataPerWeek);
   })
   .on("error", function (error) {
     console.log(error.message);
